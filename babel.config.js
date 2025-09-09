a7a9fbc7bfc040b9f85c8b398f142d3fcdd71c3a
Yujie Huang <1185679154@qq.com>
2025-09-09 12:57:15 -0400

    1. debug. 2. change the icon and the name of app is changed to "ClimMate".

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,20 @@
+// babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ["babel-preset-expo"],
+    presets: ['babel-preset-expo'],
     plugins: [
-      "react-native-reanimated/plugin", // ⚡ 必须放在最后
+      'expo-router/babel',      // ← Expo Router 的 babel 插件要保留
+      ['module-resolver', {
+        root: ['./'],
+        alias: {
+          '@': './app',          // ← 关键：把 @ 指向 app 目录
+          '@components': './app/components',
+          '@contexts': './app/contexts',
+          '@lib': './app/lib',
+        },
+        extensions: ['.tsx', '.ts', '.js', '.json'],
+      }],
     ],
   };
 };
